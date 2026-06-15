4a5d3ee4953b6938d6dc24672ccc6b830eac2739
alexgould17 <alexgould17@gmail.com>
2026-06-14 21:40:02 -0500

    Quickly documented the two Error classes in utilities.py.

--- utilities.py
+++ utilities.py
@@ -1,10 +1,10 @@
-
-
 class CyclicalError(Exception):
+    """Error representing a cycle in a graph that should be acyclical."""
     def __init__(self, msg: str):
         super(msg)
 
 
 class AlreadyExistsError(Exception):
+    """Error representing a duplicate creation of an object that shouldn't have duplicates."""
     def __init__(self, msg: str):
         super(msg)
